0c5bd8a6c83d7db437bdeab4e38e16082b7edb00
Farhad Ullah Rezwan <farhadullahrezwan@dyn-118-138-37-37.its.monash.edu.au>
2019-12-08 17:17:45 +1100

    Assignment 2

---
+++ LinkedinLearning/Exercise Files/02_04/script.js
@@ -0,0 +1,2 @@
+var date = new Date();
+document.body.innerHTML = "<h> Today is: " + date + "</h>"
---
+++ LinkedinLearning/Exercise Files/03_07/script.js
@@ -0,0 +1,5 @@
+var pens;
+pens = ["red", "blue", "blue", "orage"]
+console.log(pens)
+var frouthPen = pens[3]
+console.log(frouthPen)
