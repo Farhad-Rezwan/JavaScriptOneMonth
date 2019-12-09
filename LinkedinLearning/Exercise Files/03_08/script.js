d9e97f95339f4b6a8218c7d0b3e1d38bdee0f5a3
Farhad Ullah Rezwan <farhadullahrezwan@dyn-118-138-37-37.its.monash.edu.au>
2019-12-09 16:35:19 +1100

    learning Array in detail JavaScript

--- LinkedinLearning/Exercise Files/03_08/script.js
+++ LinkedinLearning/Exercise Files/03_08/script.js
@@ -3,29 +3,33 @@ pens = ["red", "blue", "green", "orange"];
 
 console.log("Before: ", pens);
 
-// PROPERTIES:
-// Get a property of an object by name:
+// // PROPERTIES:
+// // Get a property of an object by name:
 // console.log("Array length: ", pens.length);
-
-// METHODS:
-// Reverse the array:
+//
+// // METHODS:
+// // Reverse the array:
 // pens.reverse();
-
-// Remove the first value of the array:
+// console.log(pens)
+//
+// // Remove the first value of the array:
 // pens.shift();
-
-// Add comma-separated list of values to the front of the array:
+//
+// // Add comma-separated list of values to the front of the array:
 // pens.unshift("purple", "black");
 
-// Remove the last value of the array:
+// // Remove the last value of the array:
 // pens.pop();
-
-// Add comma-separated list of values to the end of the array:
+// console.log(pens);
+//
+//
+// // Add comma-separated list of values to the end of the array:
 // pens.push("pink", "prussian blue");
-
-// Find the specified position (pos) and remove n number of items from the array. Arguments: pens.splice(pos,n):
-// pens.splice(pos, n) // Starts at the seccond item and removes two items.
-
+// console.log(pens)
+// // Find the specified position (pos) and remove n number of items from the array. Arguments: pens.splice(pos,n):
+// pens.splice(1, 3) // Starts at the seccond item and removes two items.
+//
+//
 // console.log("After: ", pens);
 
 // Create a copy of an array. Typically assigned to a new variable:
@@ -33,11 +37,11 @@ console.log("Before: ", pens);
 // console.log("New pens: ", newPens);
 
 // Return the first element that matches the search parameter after the specified index position. Defaults to index position 0. Arguments: pens.indexOf(search, index):
-// var result = pens.indexOf(search, index);
+// var result = pens.indexOf("orange", 0);
 // console.log("The search result index is: ", result);
 
 // Return the items in an array as a comma separated string. The separator argument can be used to change the comma to something else. Arguments: pens.join(separator):
-// var arrayString = pens.join(separator);
+// var arrayString = pens.join(" ");
 // console.log("String from array: ", arrayString);
 
 // MDN documentation for Array:
