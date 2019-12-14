84d4b4f448513fac04b60829c73c53480796fa2b
Farhad Ullah Rezwan <farhadullahrezwan@dyn-118-138-37-37.its.monash.edu.au>
2019-12-14 20:10:11 +1100

    Atom JSON

--- One Month/w3/SoundCloudPlayer/javascript/main.js
+++ One Month/w3/SoundCloudPlayer/javascript/main.js
@@ -1,1 +1,38 @@
-console.log("Is this working?")
+// 1. Search SoundCloud
+
+
+
+
+
+
+
+//2. query sound SoundCloud using API
+
+
+
+SC.initialize({
+  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+});
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+SC.get('/tracks', {
+  q: 'buskers'//, license: 'cc-by-sa'
+}).then(function(tracks) {
+  console.log(tracks);
+});
+
+
+
+
+
+
+//3. Display the cards
+
+
+
+
+
+
+
+
+//4. Add to playlist to play
