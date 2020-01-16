142f13561a6462742fd62e271a09dc2e480c024d
Farhad34 <frez0003@student.monash.edu>
2020-01-16 19:25:02 +1100

    Analog Clock Linkedin Learning

--- LinkedinLearning/Exercise Files/04_12/script.js
+++ LinkedinLearning/Exercise Files/04_12/script.js
@@ -1,22 +1,71 @@
-function giveMeEms(pixels){
-	var baseValue = 16;
-
-	function doTheMath(){
-		return pixels/baseValue;
+//clousers Functions inside a function, that relies on outer function to work
+function giveMeEms2(pixels){
+	var baseValue2 = 16;
+	function doTheMath2 (){
+		return pixels/baseValue2;
+		
 	}
-	return doTheMath;
+	return doTheMath2;
 }
 
-var smallSize = giveMeEms(12);
-var mediumSize = giveMeEms(18);
-var largeSize = giveMeEms(24);
-var xLargeSize = giveMeEms(32);
+var smallSize1 = giveMeEms2(12);
+var mediumSize1 = giveMeEms2(15);
+var largeSize1 = giveMeEms2(24);
+var xLargeSize1 = giveMeEms2(32);
+
+console.log("Small Size: ", smallSize1);
+console.log("Medium Size: ", mediumSize1);
+console.log("large Size: ", largeSize1());
+console.log("xLarge Size: ", xLargeSize1());
 
 
-console.log("Small Size: ", smallSize());
-console.log("Medium Size: ", mediumSize());
-console.log("Large Size: ", largeSize());
-console.log("Extra Large Size: ", xLargeSize());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function giveMeEms(pixels){
+// 	var baseValue = 16;
+//
+// 	function doTheMath(){
+// 		return pixels/baseValue;
+// 	}
+// 	return doTheMath;
+// }
+//
+// var smallSize = giveMeEms(12);
+// var mediumSize = giveMeEms(18);
+// var largeSize = giveMeEms(24);
+// var xLargeSize = giveMeEms(32);
+//
+//
+// console.log("Small Size: ", smallSize());
+// console.log("Medium Size: ", mediumSize());
+// console.log("Large Size: ", largeSize());
+// console.log("Extra Large Size: ", xLargeSize());
 
 
 
